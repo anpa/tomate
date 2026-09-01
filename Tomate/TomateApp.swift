ad699072c3a0e81ac7847ce46f6b17bf3d58c322
André Alves <andrenpalves@gmail.com>
2026-09-01 16:23:34 +0200

    Initial Commit

---
+++ Tomate/TomateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomateApp.swift
+//  Tomate
+//
+//  Created by André Alves on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TomateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
